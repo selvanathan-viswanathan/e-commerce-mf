544e20a9f5e14b461753021e27db26086030c485
selvanathan-viswanathan <selvanathaan@gmail.com>
2025-08-22 02:19:55 +0530

    Product-list component: Added Webpack config for dev and build
      - added ts- loader for transpiling
      - a page displaying checkout

---
+++ product-list/src/ProductList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductList: React.FC = () => {
+  return (
+    <div>
+      <h1>Product list</h1>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ product-list/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: { port: 3001 },
+  output: {
+    publicPath: "auto",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "productlist",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductList": "./src/ProductList",
+      },
+      shared: ["react", "react-dom"],
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+};

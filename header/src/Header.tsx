f13f59216322cfb6d2c5f74867acc62fe7fa6fd2
selvanathan-viswanathan <selvanathaan@gmail.com>
2025-08-23 23:11:41 +0530

    Added Header component

--- header/src/Header.tsx
+++ header/src/Header.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { FC } from "react";
+import { FaCreditCard, FaShoppingCart } from "react-icons/fa";
+import "./Header.css";
 
-const Header: React.FC = () => {
+const Header: FC = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="header">
+      {/* Left: Logo + Name */}
+      <div className="logo-container">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/263/263142.png" // random shop logo
+          alt="App Logo"
+          className="logo"
+        />
+        <span className="app-name">E-commerce</span>
+      </div>
+
+      {/* Right: Icons */}
+      <div className="nav-icons">
+        <button className="icon-btn" aria-label="Cart">
+          <FaShoppingCart />
+        </button>
+        <button className="icon-btn" aria-label="Checkout">
+          <FaCreditCard />
+        </button>
+      </div>
+    </header>
   );
 };
 
